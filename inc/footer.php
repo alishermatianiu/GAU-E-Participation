b5f21971c3a9b4359debbd572648236f610163eb
Mujtaba <45472757+MojiFJ@users.noreply.github.com>
2019-05-09 16:02:10 +0300

    Revert "H sezer patch 1"

--- inc/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Include CSS -->
-<link href="./css/bootstrap.min.css" rel="stylesheet"><!-- Bootstrap CSS -->
-<link href="./css/style.css" rel="stylesheet"><!-- Custom CSS -->
-<!-- Include Google font -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,300,600">
-
-
-</body>
-</html>
